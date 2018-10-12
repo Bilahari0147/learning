8aadf6d50630f0365a65669ccf961ba72a8f5a73
Bilahari0147 <hari.iitp@gmail.com>
2018-10-12 15:40:47 +0530

    Angular

---
+++ my-first-app/src/app/education/education.component.ts
@@ -0,0 +1,49 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: '.app-education',
+    templateUrl: './education.component.html',
+    styles: [`
+    .verified{
+        color: white;}
+    `]
+})
+
+export class EducationComponent{
+
+    name: String='Bilahari Goruputi';
+    highestQualification: String='B tech';
+    passyear:number= 2015;
+
+    getName(){
+        return this.name; 
+    }
+
+    allowNewEducation=false;
+    educationStatus='not verified';
+    educationName='Btech';
+    educationadded=false;
+    constructor(){
+        setTimeout(() => {
+            this.allowNewEducation=true;
+        },3000);
+  
+    }
+
+
+    onAddEducation(){
+        this.educationadded=true;
+        this.educationStatus='Education Added! Name is:'+this.educationName;
+    }
+    onUpdateEducationName(){
+        this.educationName=(<HTMLInputElement>event.target).value;
+    }
+    ngOnInit(){
+        this.educationStatus=Math.random() > 0.5? 'verified':'not verified';
+    }
+
+    getColor(){
+        return this.educationStatus==='verified' ? 'green':'red';
+    }
+    
+}
